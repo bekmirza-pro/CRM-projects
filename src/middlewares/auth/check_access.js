df03ed84ec4482e5884a9ac2d7cf79f25c707cb5
asarvarjon <amriyevsarvarjon@yandex.com>
2022-08-04 07:27:23 +0500

    add new fetur

---
+++ src/middlewares/auth/check_access.js
@@ -0,0 +1,42 @@
+const sequelize = require("../../sequlize/db");
+const {
+    user_roles,
+    modules,
+    roles
+} = sequelize.models
+
+const check_access = (module_name) => {
+    return async (req, res, next) => {
+        try {
+            const {
+                user
+            } = req 
+
+            const all_modules = await user_roles.findAll({
+                where: {
+                    user_id: user.id,
+                }, 
+                include: [
+                    {
+                        model: roles,
+                        include: [
+                            {
+                                model: modules
+                            }
+                        ]
+                    }
+                ],
+                raw: true
+            });  
+
+            const existance = all_modules.find(e => e["role.modules.name"] == module_name);  
+            if (!existance) throw new res.error(400, "Access denied!")
+
+            next()
+
+        } catch (error) { 
+            next(error)
+        }
+    }
+}
+module.exports = check_access
