c43440c5248b884b2e9e10f967036d481115802e
asarvarjon <amriyevsarvarjon@yandex.com>
2022-08-04 07:36:17 +0500

    add  new fetu

--- src/sequelize/init_module.js
+++ src/sequelize/init_module.js
@@ -2,6 +2,27 @@ const sequelize = require("./db");
 const { roles } = sequelize.models;
 // const { generateHash } = require("../../src/modules/bcrypt");
 
+const admin_modules_data = [
+  "get_all_users",
+  "get_user", 
+  "delete_user", 
+
+  "get_all_unverified_users",
+  "verify_user",
+
+  "ban_user", 
+  "get_roles",
+
+  "create_category",
+  "update_category",
+  "delete_category",
+
+  "get_all_tags",
+  "create_tag",
+  "delete_tag",
+  "add_tag",
+]
+
 module.exports = async () => {
   const role_count = await roles.count();
   if (role_count == 0) {
@@ -17,6 +38,28 @@ module.exports = async () => {
       })
     }
   }
+
+  const modulesCount = await modules.count();
+
+  if (modulesCount == 0) {
+
+      // admin modules
+
+      const admin = await roles.findOne({
+          where: {
+              name: "admin"
+          },
+          raw: true
+      })
+
+      admin_modules_data.forEach(async e => {
+          let amodule = await modules.create({
+              name: e,
+              role_id: admin.id
+          })
+      })
+
+    }
 
 };
 
